1b0245cb4f786e792d31f3fdd621468cbf4d30de
Jacob Chae <jacob.chae@mobiledefense.com>
2016-03-15 15:13:10 -0400

    Mock out unnecessary network  calls

--- spec/vulnerable_version_check_spec.rb
+++ spec/vulnerable_version_check_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe VulnerableVersionCheck do
   describe '#call' do
     context 'when there are no vulnerable gems' do
       let(:deps) { safe_parsed_results }
+      before do
+        allow_any_instance_of(GemnasiumClient).to receive(:vulnerable?)
+          .and_return(false)
+      end
 
       it 'returns an empty list' do
         expect(subject.call(deps)).to be_empty
@@ -13,7 +17,6 @@ RSpec.describe VulnerableVersionCheck do
 
     context 'when there are vulnerable gems' do
       let(:deps) { vulnerable_parsed_results }
-
       before do
         allow_any_instance_of(GemnasiumClient).to receive(:vulnerable?) do |obj, arg|
           arg == vulnerable_gem ? true : false
