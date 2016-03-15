d08ab86e895c91aadd32d7ef27c4c805a19477ea
Jacob Chae <jacob.chae@mobiledefense.com>
2016-03-15 15:04:15 -0400

    Ensure GemnasiumClient can scrape vulnerability info from Gemnasium

---
+++ spec/gemnasium_client_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'gemnasium/gemnasium_client'
+
+RSpec.describe GemnasiumClient do
+  describe '#vulnerable?', speed: 'slow' do
+    context 'when the gem is not vulnerable' do
+      let(:gem) { safe_parsed_results.first }
+
+      it 'returns false' do
+        gem = safe_parsed_results.last
+        expect(subject.vulnerable?(gem)).to be_falsey
+      end
+    end
+
+    context 'when the gem is vulnerable' do
+      let(:gem) { vulnerable_parsed_results.last }
+
+      it 'returns true' do
+        expect(subject.vulnerable?(gem)).to be_truthy
+      end
+    end
+  end
+end
