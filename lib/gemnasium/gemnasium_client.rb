e8abc7e33ab9eb2cc8f84c2f7880d9d27ec2d6c6
Jacob Chae <jacob.chae@mobiledefense.com>
2016-03-15 15:04:58 -0400

    GemnasiumClient can scrape vulnerability info from Gemnasium

--- lib/gemnasium/gemnasium_client.rb
+++ lib/gemnasium/gemnasium_client.rb
@@ -1,4 +1,24 @@
+require 'open-uri'
+require 'nokogiri'
+
 class GemnasiumClient
-  def check(gem_name:, version:)
+  def vulnerable?(gem_name:, version:)
+    uri = client_url(gem_name, version)
+    query_gemnasium(uri)
+  end
+
+  private
+
+  def gem_vulnerable?(doc)
+    doc.css('div.accordion.advisory.affected').count >= 1
+  end
+
+  def query_gemnasium(uri)
+    doc = Nokogiri::HTML(open(uri))
+    gem_vulnerable?(doc)
+  end
+
+  def client_url(gem_name, version)
+    "https://gemnasium.com/gems/#{gem_name}/versions/#{version}"
   end
 end
